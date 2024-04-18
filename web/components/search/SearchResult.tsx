007d0b63f1aa3d3822dd7a3be9fe0f264d18cdbc
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-04-17 18:59:40 -0700

    style: update search modal spacing

--- web/components/search/SearchResult.tsx
+++ web/components/search/SearchResult.tsx
@@ -1,6 +1,6 @@
 export default function SearchResult() {
   return (
-    <div className="first:!bg-cyan-350/80 hover:bg-gray-750/40 mt-1 flex items-center justify-start rounded-lg py-2">
+    <div className="first:!bg-cyan-350/80 hover:bg-gray-750/40 mt-1.5 flex items-center justify-start rounded-md py-2">
       <button className="flex w-full cursor-default items-center outline-none">
         Movie
       </button>
--- web/components/search/SearchResultList.tsx
+++ web/components/search/SearchResultList.tsx
@@ -2,7 +2,7 @@ import SearchResult from "./SearchResult";
 
 export default function SearchResultList() {
   return (
-    <div className="cursor-default select-none border-t border-[#434343] pt-1 outline-none">
+    <div className="mb-0.5 mt-2.5 cursor-default select-none border-t border-[#434343] pt-1 outline-none">
       <SearchResult />
       <SearchResult />
       <SearchResult />
