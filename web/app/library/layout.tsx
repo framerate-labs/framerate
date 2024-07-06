b326ab82a29f79b6477c7ebee8e9df8c96a4c3cb
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-07-06 11:26:28 -0700

    feat: filter library by media type

--- web/app/library/layout.tsx
+++ web/app/library/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { type ReactNode } from "react";
 
 type LibraryLayoutProps = {
@@ -7,8 +8,30 @@ type LibraryLayoutProps = {
 export default function LibraryLayout({ children }: LibraryLayoutProps) {
   return (
     <div className="px-3 pb-3 pt-20 md:px-0 md:pt-32">
-      <h2 className="text-lg font-medium md:text-xl">Library</h2>
-      <section className="mt-3">{children}</section>
+      <div className="flex justify-between">
+        <h2 className="text-lg font-medium md:text-xl">Library</h2>
+        <div>
+          <Link
+            href="/library?filter=film"
+            className="mr-2.5 w-[61.33px] rounded bg-gray-750 bg-gradient-to-b from-zinc-200 via-zinc-200 to-zinc-200/20 bg-clip-text px-2 py-1 text-center text-transparent ring-2 ring-gray-750/70"
+          >
+            Film
+          </Link>
+          <Link
+            href="/library?filter=series"
+            className="mr-2.5 w-[61.33px] rounded bg-gray-750 bg-gradient-to-b from-zinc-200 via-zinc-200 to-zinc-200/20 bg-clip-text px-2 py-1 text-center text-transparent ring-2 ring-gray-750/70"
+          >
+            Series
+          </Link>
+          <Link
+            href="/library"
+            className="rounded bg-gray-750 bg-gradient-to-b from-zinc-200 via-zinc-200 to-zinc-200/20 bg-clip-text px-2 py-1 text-center text-transparent ring-2 ring-gray-750/70"
+          >
+            All
+          </Link>
+        </div>
+      </div>
+      <section className="mt-4">{children}</section>
     </div>
   );
 }
--- web/app/library/page.tsx
+++ web/app/library/page.tsx
@@ -8,24 +8,43 @@ import PosterGrid from "@/components/ui/PosterGrid";
 import { getMovies } from "@/lib/movieReview";
 import { getSeries } from "@/lib/seriesReview";
 
-export default function Library() {
+export default function Library({
+  searchParams,
+}: {
+  searchParams: Record<string, "film" | "series" | "animation" | "none">;
+}) {
   const [reviews, setReviews] = useState<Review[]>();
+
+  const filterMode = searchParams.filter || "none";
 
   useEffect(() => {
     (async () => {
       const movieResult = await getMovies();
       const tvResult = await getSeries();
 
-      if (movieResult && tvResult) {
-        const result = movieResult.concat(tvResult);
+      movieResult?.sort(
+        (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
+      );
+      tvResult?.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
 
-        if (result.length > 0) {
-          result.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
-          setReviews(result);
+      if (movieResult && tvResult) {
+        if (filterMode === "none") {
+          const result = movieResult.concat(tvResult);
+
+          if (result.length > 0) {
+            result.sort(
+              (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
+            );
+            setReviews(result);
+          }
+        } else if (filterMode === "film") {
+          movieResult.length > 0 && setReviews(movieResult);
+        } else if (filterMode === "series") {
+          tvResult.length > 0 && setReviews(tvResult);
         }
       }
     })();
-  }, []);
+  }, [filterMode]);
 
   return <PosterGrid reviews={reviews} />;
 }
