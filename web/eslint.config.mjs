c08a2cb734d5e0edc9132b5d733c250af30af488
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-12-16 21:25:38 -0800

    build: update estlint

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,16 +1,107 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import boundaries from 'eslint-plugin-boundaries';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      boundaries,
+    },
 
-export default eslintConfig;
+    settings: {
+      'boundaries/include': ['src/**/*'],
+
+      'boundaries/elements': [
+        {
+          mode: 'full',
+          type: 'shared',
+
+          pattern: [
+            'src/components/**/*',
+            'src/data/**/*',
+            'src/drizzle/**/*',
+            'src/hooks/**/*',
+            'src/lib/**/*',
+            'src/server/**/*',
+          ],
+        },
+        {
+          mode: 'full',
+          type: 'feature',
+          capture: ['featureName'],
+          pattern: ['src/features/*/**/*'],
+        },
+        {
+          mode: 'full',
+          type: 'app',
+          capture: ['_', 'fileName'],
+          pattern: ['src/app/**/*'],
+        },
+        {
+          mode: 'full',
+          type: 'neverImport',
+          pattern: ['src/*', 'src/tasks/**/*'],
+        },
+      ],
+    },
+
+    rules: {
+      'boundaries/no-unknown': ['error'],
+      'boundaries/no-unknown-files': ['error'],
+
+      'boundaries/element-types': [
+        'error',
+        {
+          default: 'disallow',
+
+          rules: [
+            {
+              from: ['shared'],
+              allow: ['shared'],
+            },
+            {
+              from: ['feature'],
+
+              allow: [
+                'shared',
+                [
+                  'feature',
+                  {
+                    featureName: '${from.featureName}',
+                  },
+                ],
+              ],
+            },
+            {
+              from: ['app', 'neverImport'],
+              allow: ['shared', 'feature'],
+            },
+            {
+              from: ['app'],
+
+              allow: [
+                [
+                  'app',
+                  {
+                    fileName: '*.css',
+                  },
+                ],
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
