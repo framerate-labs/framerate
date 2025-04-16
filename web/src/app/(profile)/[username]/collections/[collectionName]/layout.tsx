5b3ac2f0a7610788ceffd307add8ae2f5a5d60fe
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-04-16 14:21:51 -0700

    set up monorepo

--- web/src/app/(profile)/[username]/collections/[collectionName]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-export default async function CollectionLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <div className="animate-fade-in">{children}</div>;
-}
--- web/src/features/polar/server/tiers.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "@/polar";
-
-export const { result } = await api.products.list({
-  isArchived: false, // Only fetches published and active products
-});
