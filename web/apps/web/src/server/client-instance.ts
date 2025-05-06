c3e0dabdcb3e425e56ae5dd503445ee140df9c77
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-05-05 17:47:08 -0700

    web: update server url

--- web/apps/web/src/server/client-instance.ts
+++ web/apps/web/src/server/client-instance.ts
@@ -2,7 +2,13 @@ import type { App } from "@server/index";
 
 import { treaty } from "@elysiajs/eden";
 
-export const client = treaty<App>("localhost:8000", {
+let serverUrl = import.meta.env.VITE_BETTER_AUTH_URL ?? "localhost:8000";
+
+if (import.meta.env.DEV) {
+  serverUrl = import.meta.env.VITE_BETTER_AUTH_DEV_URL ?? "localhost:8000";
+}
+
+export const client = treaty<App>(serverUrl, {
   fetch: {
     credentials: "include",
   },
