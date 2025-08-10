f7695aa5a10ea444c1b37e3443433e9aa30bf9f1
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-08-10 16:03:51 -0700

    add packages and set up eslint & prettier

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,27 +1,25 @@
-import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { createRootRoute, HeadContent, Scripts } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import Header from "../components/Header";
-
-import appCss from "@/styles/app.css?url";
+import appCss from '@/styles/app.css?url';
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        charSet: "utf-8",
+        charSet: 'utf-8',
       },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
       },
       {
-        title: "TanStack Start Starter",
+        title: 'TanStack Start Starter',
       },
     ],
     links: [
       {
-        rel: "stylesheet",
+        rel: 'stylesheet',
         href: appCss,
       },
     ],
@@ -37,7 +35,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <Header />
         {children}
         <TanStackRouterDevtools />
         <Scripts />
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,39 +1,82 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { Fingerprint, Ticket } from 'lucide-react';
 
 export const Route = createFileRoute('/')({
-  component: App,
-})
+  component: LandingPage,
+});
 
-function App() {
+function LandingPage() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
+    <div className="relative h-full">
+      <header>
+        <nav className="flex items-center justify-between pt-8">
+          <Link to="/">
+            <h1 className="text-3xl font-extrabold">FrameRate</h1>
+          </Link>
+
+          <div className="flex items-center gap-10 font-semibold">
+            <Link to="/" className="group/login flex items-center gap-2">
+              <span className="text-gray group-hover/login:text-foreground transition-colors duration-200">
+                <Fingerprint size={18} />
+              </span>
+              Login
+            </Link>
+
+            <Link
+              to="/"
+              className="pill-bg group/signup peer flex items-center gap-2 rounded-full border border-transparent px-3.5 py-0.5"
+            >
+              <span className="text-gray group-hover/signup:text-foreground transition-colors duration-200">
+                <Ticket size={18} />
+              </span>
+              Create free account
+            </Link>
+          </div>
+        </nav>
       </header>
+
+      <main className="mx-auto px-20">
+        {/* Images */}
+        <section className="absolute top-0 right-0 left-0 -z-10 mx-auto w-full items-center">
+          <div className="absolute top-24 right-0 left-0 mx-auto w-[90%]">
+            <img
+              src="https://image.tmdb.org/t/p/original/uDgy6hyPd82kOHh6I95FLtLnj6p.jpg"
+              alt="Joel and Ellie from the TV series The Last of Us, released in 2023."
+              width={1920}
+              height={1080}
+              decoding="async"
+              loading="eager"
+              className="animate-fade-in"
+            />
+            <div className="marketing-image-fade absolute top-0 right-0 left-0 z-10 size-full"></div>
+            <div className="easing-gradient absolute top-0 right-0 left-0 size-full"></div>
+            <span className="text-gray/40 absolute top-1/2 -right-4 z-10 -rotate-90 text-sm font-medium text-nowrap">
+              The Last of Us (2023)
+            </span>
+          </div>
+          <div className="side-bottom-fade absolute top-0 right-0 left-0 z-10 size-full"></div>
+        </section>
+
+        {/* Hero Text */}
+        <section className="mb-10 pt-[475px] text-center">
+          <div className="mb-8">
+            <h2 className="text-[32px] font-bold tracking-tight">
+              Movie madness, managed.
+            </h2>
+            <p className="mt-3 text-[18px] font-medium tracking-wide">
+              Track your cinematic journey.
+            </p>
+          </div>
+
+          {/* CTA */}
+          <Link
+            to="/"
+            className="hover:bg-foreground text-foreground relative rounded-full border border-white/70 px-12 py-1.5 font-bold tracking-wide shadow-sm transition-all duration-200 ease-in hover:text-neutral-800"
+          >
+            Get started
+          </Link>
+        </section>
+      </main>
     </div>
-  )
+  );
 }
