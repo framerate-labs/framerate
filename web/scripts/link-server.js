513299a0a80823ac09ec0a5e236bcb2bb6931da6
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-10-09 18:39:27 -0400

    prepare SvelteKit rewrite

--- web/scripts/link-server.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from 'child_process';
-import { existsSync } from 'fs';
-
-const local = '../../server';
-
-if (existsSync(local)) {
-  console.log('Linking @framerate/server to local ../../server');
-  execSync('Bun link ../../server', { stdio: 'inherit' });
-}
